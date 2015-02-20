5ffa806e1f1fbcf93b9eb991781bf6a6674be129
aqutw <aquarian.ex+github@gmail.com>
2015-02-20 23:28:02 +0800

    FINISH test sendgrid send-mail

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var sendgrid  = require('sendgrid')("USERNAME", "PASSWORD"); //create USERNAME & PASSWORD from https://sendgrid.com/credentials
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  sendgrid.send({
+    to:       'prosai.net+sendgridtest@gmail.com',
+    from:     'aquarian.ex@gmail.com',
+    subject:  'Hello World中文',
+    text:     'My first中文測試 email through SendGrid.'
+  }, function(err, json) {
+    if (err) { return console.error(err); }
+    console.log(json);
+  });
 });
 
 module.exports = router;
